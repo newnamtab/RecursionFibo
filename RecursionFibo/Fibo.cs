3d9f21f5646c818e0d521db378b9b5e5b1cca152
Søren Hansen <newnamtab@yahoo.dk>
2017-02-09 19:28:37 +0100

    Some FIBO solution

---
+++ RecursionFibo/Fibo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionFibo
+{
+    class Fibo
+    {
+        public int FiboLoop(int input)
+        {
+            int currentNumber = 0;
+            int oldNumber1 = 1;
+            int oldNumber2 = 1;
+
+            if (input == 0)
+            {
+
+                return 0;
+            }
+            if (input < 3)
+            {
+
+                return 1;
+            }
+            else
+            {
+
+                for (int i = 2; i < input; i++)
+                {
+                    currentNumber = oldNumber1 + oldNumber2;
+                    oldNumber2 = oldNumber1;
+                    oldNumber1 = currentNumber;
+
+                }
+                return currentNumber;
+            }
+
+        }
+
+        public int CalcFibo(int input)
+        {
+            if (input == 0)
+            {
+                return 0;
+            }
+            if (input < 3)
+            {
+                return 1;
+            }
+            else
+            {
+                return CalcFibo(input - 1) + CalcFibo(input - 2);
+            }
+
+        }
+    }
+}
---
+++ RecursionFibo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionFibo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibo myFibo = new Fibo();
+            Console.WriteLine(myFibo.FiboLoop(10));
+            Console.WriteLine(myFibo.CalcFibo(10));
+
+        }
+    }
+}
